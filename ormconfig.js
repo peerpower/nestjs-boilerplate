2038a9727cccf6f5287b5765f5b73882984a0b09
นพสุทธิ์ เศวตมาลานนท์ <amp@Macintosh.local>
2022-04-18 15:13:25 +0700

    AI-141: Update boilerplate

--- ormconfig.js
+++ ormconfig.js
@@ -1,23 +1,23 @@
-const dotenv = require('dotenv');
+const { readFileSync } = require('fs');
+const yaml = require('js-yaml');
+const { join } = require('path');
 
-dotenv.config();
+const YAML_CONFIG_FILENAME = 'config.yaml';
 
-const {
-  DB_TYPE,
-  DB_HOST,
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_DATABASE,
-} = process.env;
+const config = yaml.load(readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'));
+
+const { DB_TYPE, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_PORT, DB_DATABASE } = config;
 
 module.exports = {
-  type: DB_TYPE,
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-  migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
-  entities: [__dirname + '/src/**/*.entity.{ts,js}'],
+    type: DB_TYPE,
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
+    entities: [__dirname + '/src/**/*.entity.{ts,js}'],
+    cli: {
+        migrationsDir: __dirname + '/src/migrations/',
+    },
 };
--- src/modules/main/app.service.ts
+++ src/modules/main/app.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from './../config';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private config: ConfigService) {}
+    constructor(private config: ConfigService) {}
 
-  root(): string {
-    return this.config.get('APP_URL');
-  }
+    root(): string {
+        return this.config.get('APP_URL');
+    }
 }
